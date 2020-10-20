0c166589eb04c02849b3ab78372e92f273023fb0
Jonah Fleishhacker <jfleishhacker@uchicago.edu>
2020-10-20 18:14:17 -0500

    initial swipe test

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+
+class SomeComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      myText: 'I\'m ready to get swiped!',
+      gestureName: 'none',
+    };
+  }
+
+  onSwipeUp(gestureState) {
+    this.setState({myText: 'You swiped up!'});
+  }
+
+  onSwipeDown(gestureState) {
+    this.setState({myText: 'You swiped down!'});
+  }
+
+  onSwipeLeft(gestureState) {
+    this.setState({myText: 'You swiped left!'});
+  }
+
+  onSwipeRight(gestureState) {
+    this.setState({myText: 'You swiped right!'});
+  }
+
+
+  render() {
+
+    const config = {
+      velocityThreshold: 0.3,
+      directionalOffsetThreshold: 80
+    };
+
+    return (
+      <GestureRecognizer
+        onSwipeUp={(state) => this.onSwipeUp(state)}
+        onSwipeDown={(state) => this.onSwipeDown(state)}
+        onSwipeLeft={(state) => this.onSwipeLeft(state)}
+        onSwipeRight={(state) => this.onSwipeRight(state)}
+        config={config}
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+        >
+        <Text>{this.state.myText}</Text>
+      </GestureRecognizer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default SomeComponent;
